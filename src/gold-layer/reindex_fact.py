e9bd2027157830b8c0a5d2b46fd617e6c64d283a
cltj <tj@cloudlink.ai>
2022-10-06 21:05:52 +0200

    sort blobs

--- src/gold-layer/reindex_fact.py
+++ src/gold-layer/reindex_fact.py
@@ -1,4 +1,34 @@
 import sys
 sys.path.insert(0,'/mnt/c/dev/cl/pipeline')
 from src.config import My_Config as cfg
+from download_tables import get_data
 import pandas as pd
+
+
+# for each table in silver blob
+# if fact, store it in memory
+def get_blobs(word):
+    from azure.storage.blob import BlobServiceClient, BlobBlock
+    CONNECTION_STRING = cfg.storage_connection_string()
+    BILLING_CONTAINER = cfg.storage_container_name_2()
+    blob_service_client =  BlobServiceClient.from_connection_string(CONNECTION_STRING)
+    container_client=blob_service_client.get_container_client(BILLING_CONTAINER)
+    blob_list = container_client.list_blobs()
+    for blob in blob_list:
+        print("\t" + blob.name)
+    return blob_list
+
+
+def get_fact():
+    blobs = get_blobs()
+    for blob in blobs:
+        if "dim" not in blob:
+            fact  = blob
+            df = pd.DataFrame(fact)
+            return df
+        if "fact" not in blob:
+            continue
+
+#for each table in silver
+# if dim table, store it
+# for each index value in dim table, if col value(dim) = col vaule(fact), insert index vaule
