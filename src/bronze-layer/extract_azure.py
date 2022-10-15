a0511c84153cd8660d3c3fdc01b46e90ae4190d9
cltj <tj@cloudlink.ai>
2022-10-15 13:56:33 +0200

    fix azure on bronze

--- src/bronze-layer/extract_azure.py
+++ src/bronze-layer/extract_azure.py
@@ -1,27 +1,31 @@
-from multiprocessing.connection import wait
+import os, time
+import pandas as pd
+from azure.storage.blob import ContainerClient
+
+# Env-vars
+LOCAL_FILES_PATH = os.environ.get("LOCAL_FILES_PATH")
+CONTAINERNAME = os.environ.get("CONTAINERNAME")
+ACCOUNTNAME = os.environ.get("ACCOUNTNAME")
+QUERYSTRING = os.environ.get("QUERYSTRING")
+CONNECTION_STRING = os.environ.get("CONNECTION_STRING")
+
+
+def blobs():
+    container_client =  ContainerClient.from_connection_string(CONNECTION_STRING, CONTAINERNAME)
+    blob_list = container_client.list_blobs()
+    return blob_list
 
 
 def azure_billing():
-    from azure.identity import ClientSecretCredential
-    from azure.mgmt.consumption import ConsumptionManagementClient
-    import pandas as pd
-    import sys, os, time
-    sys.path.insert(0,'/mnt/c/dev/cl/pipeline')
-    from src.config import My_Config as cfg
-    
-    # This needs an app registration in azure.
-    credential = ClientSecretCredential(
-        tenant_id=cfg.az_tenant_id(), 
-        client_id=cfg.az_client_id(), 
-        client_secret=cfg.az_client_secret()
-        )
-    LOCAL_FILES_PATH = cfg.local_files_path()
-    client = ConsumptionManagementClient(credential=credential,subscription_id=cfg.az_subscription_id())
-    scope='/subscriptions/' + str(cfg.az_subscription_id()) + '/'
-    usage = client.usage_details.list(scope=scope)
+    blob_list = blobs()
     df = pd.DataFrame()
-    for item in usage:
-        df = df.append(item.as_dict(), ignore_index=True)
+    for blob in blob_list:
+        container = blob.container
+        blob_name = blob.name
+        sas_url = f"https://{ACCOUNTNAME}.blob.core.windows.net/{container}/{blob_name}{QUERYSTRING}"
+        df2 = pd.read_csv(sas_url)
+        df = pd.concat([df,df2])
+        
     isExist = os.path.exists(LOCAL_FILES_PATH + 'data/azure_data/')
     if isExist is False:
         os.mkdir(LOCAL_FILES_PATH + 'data/azure_data/')
@@ -29,3 +33,4 @@ def azure_billing():
         
     df.to_parquet(LOCAL_FILES_PATH + 'data/azure_data/Azure-Billing-Data.parquet')
     print("Azure billing data extracted!")
+    
