d5336b30880285703b295e611c6f0cf40f04f504
cltj <tj@cloudlink.ai>
2022-10-16 09:46:57 +0200

    cleanup

--- src/bronze-layer/upload_to_bronze.py
+++ src/bronze-layer/upload_to_bronze.py
@@ -1,10 +1,6 @@
-import os, sys
-sys.path.insert(0,'/mnt/c/dev/cl/pipeline')
-from src.config import My_Config as cfg
-
-    
 def upload_parquet():
     from azure.storage.blob import BlobServiceClient
+    import os
     
     CONNECTION_STRING = os.environ.get("STORAGE_CONNECTION_STRING")
     BILLING_CONTAINER = os.environ.get("STORAGE_CONTAINER_NAME_1")
@@ -14,12 +10,9 @@ def upload_parquet():
     class AzureBlobFileUploader:
         def __init__(self):
             print("Intializing AzureBlobFileUploader")
-            # Initialize the connection to Azure storage account
             self.blob_service_client =  BlobServiceClient.from_connection_string(CONNECTION_STRING)
         
         def upload_all_files_in_folder(self):
-            # Get all files with parquet extension and exclude directories
-            directories  = os.listdir(LOCAL_FILES_PATH+'data')
             gcp_data  = os.listdir(LOCAL_FILES_PATH+'data/gcp_data')
             aws_data  = os.listdir(LOCAL_FILES_PATH+'data/aws_data')
             azure_data  = os.listdir(LOCAL_FILES_PATH+'data/azure_data')
@@ -40,11 +33,11 @@ def upload_parquet():
         def upload_file(self,file_name,path, platform):
             container_path = BILLING_CONTAINER+"/"+CUSTOMER_NAME+"/"+platform
             blob_client = self.blob_service_client.get_blob_client(container=container_path, blob=file_name) # Create blob with same name as local file name
-            upload_file_path = os.path.join(path, file_name) # This needs fixing
+            upload_file_path = os.path.join(path, file_name)
             
             print(f"uploading file - {file_name}")
-            with open(upload_file_path, "rb") as data: # Uploads to cltj
+            with open(upload_file_path, "rb") as data:
                 blob_client.upload_blob(data,overwrite=True)
 
-    azure_blob_file_uploader = AzureBlobFileUploader() # Initialize class and upload files
+    azure_blob_file_uploader = AzureBlobFileUploader()
     azure_blob_file_uploader.upload_all_files_in_folder()
